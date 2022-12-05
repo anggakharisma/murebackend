4b511a5878dcff9c980ae964d17be5b59ebe35fb
angga kharisma <anggaputrakharisma@gmail.com>
2022-12-05 13:46:35 +0700

    chore: Add lombok on User

--- src/main/java/com/murebackend/murebackend/User/User.java
+++ src/main/java/com/murebackend/murebackend/User/User.java
@@ -7,21 +7,25 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
-import lombok.Data;
+import lombok.*;
 
 @Data
+@Setter
+@RequiredArgsConstructor
 public class User {
-
 	private Long id;
 
+	@NonNull
 	@NotBlank(message = "name is required")
 	private String name;
 
+	@NonNull
 	@Email(message="Please provide a valid email address")
 	@Pattern(regexp=".+@.+\\..+", message="Please provide a valid email address")
 	@NotBlank(message = "email is required")
 	private String email;
 
+	@NonNull
 	@NotBlank(message = "password is required")
 	private String password;
 	private Timestamp createdAt;
@@ -29,72 +33,8 @@ public class User {
 
 	private String imagePath;
 
-	public User() {
-	}
-
-	public User(String name, String email, String password) {
-		this.name = name;
-		this.email = email;
-		this.password = password;
-	}
-
-	public User(Long id, String name, String email, String password) {
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.password = password;
-	}
-
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
 	@Override
 	public String toString() {
 		return "User [email=" + email + ", id=" + id + ", name=" + name + "]";
-	}
-
-	public Timestamp getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Timestamp createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public Timestamp getUpdatedAt() {
-		return updatedAt;
-	}
-
-	public void setUpdatedAt(Timestamp updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-
-	public String getImagePath() {
-		return imagePath;
-	}
-	public void setImagePath(String imagePath) {
-		this.imagePath = imagePath;
 	}
 }
--- src/test/java/com/murebackend/murebackend/BaseTest.java
+++ src/test/java/com/murebackend/murebackend/BaseTest.java
@@ -1,13 +1,15 @@
 package com.murebackend.murebackend;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(properties = "spring.profiles.active:test")
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 public class BaseTest {
 
     @Autowired
